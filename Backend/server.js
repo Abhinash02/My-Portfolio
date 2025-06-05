8e3fde1f2ec24f8180e71dbceb268ac66a15b6b6
ABHINASH <145132833+Abhinash02@users.noreply.github.com>
2025-06-05 16:12:14 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -313,6 +313,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
+const Education = require('./models/Education');
 const port = process.env.PORT || 5000;
 
 // Middleware
